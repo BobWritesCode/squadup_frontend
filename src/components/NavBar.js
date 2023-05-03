f8190f40c7aa1bce9f391ab2ad62dc1e79886000
BobWritesCode <warwick_hart@hotmail.com>
2023-05-03 15:55:49 +0100

    feat: create NavBar css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../assets/logo-white-text-grey-bg.png';
-import styles from '../styles/NabBar.module.css';
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
   const homeLink = (
