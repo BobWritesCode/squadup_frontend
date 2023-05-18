d826f843bac31e1e28ad8e5db42171f5739a6499
BobWritesCode <warwick_hart@hotmail.com>
2023-05-18 12:26:15 +0100

    chore: remove redundant spinner

--- src/pages/lfg/LFGPage.js
+++ src/pages/lfg/LFGPage.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import LoadSpinner from '../../components/Spinner';
+import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MyGroups from '../../components/groups/MyGroups';
@@ -7,21 +6,17 @@ import GroupList from '../../components/groups/GroupList';
 import MyApplications from '../../components/groups/MyApplications';
 
 const LFGPage = () => {
-  const [hasLoaded, setHasLoaded] = useState(false);
-
-  useEffect(() => {
-    setHasLoaded(true);
-  }, []);
-
   return (
     <Row>
       <Col className="">
         <h2>Look for group</h2>
-        <div className='d-flex d-row'>
+        <div className="d-flex d-row">
           <MyGroups />
           <MyApplications />
         </div>
-        <div>{hasLoaded ? <GroupList /> : <LoadSpinner />}</div>
+        <div>
+          <GroupList />
+        </div>
       </Col>
     </Row>
   );
