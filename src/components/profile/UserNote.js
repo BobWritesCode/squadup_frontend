7bba33faca454ec70daaaa0231e0c01335702000
BobWritesCode <warwick_hart@hotmail.com>
2023-05-21 15:29:30 +0100

    bug: fixes #34 rewrite user note code (see more)
    - Change how target_user id is set by using useParams().
    - Add default attributes to userNote.
    - Add comment and doc-strings though.

--- src/components/profile/UserNote.js
+++ src/components/profile/UserNote.js
@@ -6,23 +6,39 @@ import LoadSpinner from '../Spinner';
 import { useParams } from 'react-router-dom';
 
 const UserNote = () => {
+  // Get url params
   const { id } = useParams();
+  // Get current user logged in.
   const currentUser = useCurrentUser();
-
+  // Use to show spinner while waiting for data from API
   const [hasLoaded, setHasLoaded] = useState(false);
-  const [userNote, setUserNote] = useState({});
+  // Use to store form data.
+  const [userNote, setUserNote] = useState({
+    id: null,
+    content: '',
+  });
 
+  /**
+   * Calls API to get data using filters. If data found set to variable.
+   */
   useEffect(() => {
+    // Checks for logged in user.
     if (currentUser) {
       const fetchData = async () => {
         try {
+          // Make API call with filter options.
           const { data } = await axiosReq.get(
             `/usernotes/?owner=${currentUser.pk}&target_user=${id}`,
           );
-          setUserNote(data.results[0]);
+          // If results found update variable.
+          if (data.results.length > 0) {
+            setUserNote(data.results[0]);
+          }
         } catch (err) {
+          // Show errors in console.
           console.log(err);
         } finally {
+          // Remove spinner.
           setHasLoaded(true);
         }
       };
@@ -30,6 +46,10 @@ const UserNote = () => {
     }
   }, [currentUser, id]);
 
+  /**
+   * Allow user to edit form.
+   * @param {*} e
+   */
   const handleUserNoteChange = (e) => {
     setUserNote(e);
   };
@@ -53,7 +73,11 @@ const UserNote = () => {
             <LoadSpinner />
           ) : (
             <p className={`${appStyles.OrangeText} text-break`}>
-              {userNote.content}
+              {userNote.content !== '' ? (
+                userNote.content
+              ) : (
+                <em>You have not left a note on this profile yet</em>
+              )}
             </p>
           )}
         </div>
