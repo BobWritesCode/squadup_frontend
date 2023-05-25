88ff7cdad4dfcea01c5b0a678d99ab81d73c3d7e
BobWritesCode <warwick_hart@hotmail.com>
2023-05-25 11:16:51 +0100

    chore: add comments/doc strings to Sign In form

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,55 +1,74 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
-// React BootStrap imports
 import Form from 'react-bootstrap/Form';
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
-// app imports
 import { setTokenTimestamp } from '../../utils/utils';
 import { axiosDefaultsBaseUrl } from '../../api/axiosDefaults';
 import { useSetCurrentUser } from '../../contexts/CurrentUserContext';
-// css imports
 import appStyles from '../../App.module.css';
 import styles from '../../styles/SignUpForm.module.css';
 import btnStyles from '../../styles/Buttons.module.css';
 import LoadSpinner from '../../components/Spinner';
 
+/**
+ * Sign in page.
+ * @return {JSX} Renders page.
+ */
 function SignInForm() {
+  // Set current user on successful authentication.
   const setCurrentUser = useSetCurrentUser();
-
+  // react-router-dom navigation control.
+  const navigate = useNavigate();
+  // Use to control data for sign in form.
   const [signInData, setSignInData] = useState({
     username: '',
     password: '',
   });
-
   const { username, password } = signInData;
-
+  // Use to any expected errors as alerts.
   const [errors, setErrors] = useState({});
+  // Use to toggle showing spinner while waiting for API to resolve.
   const [showSpinner, setShowSpinner] = useState(false);
-  const navigate = useNavigate();
 
+  /**
+   * Handles submitting form data to api and handling the response.
+   * If user is not authenticated, then expected error messages are to be shown.
+   * If use is authenticated, then set tokens, and redirect user to profile page.
+   * @param {*} event
+   */
   const handleSubmit = async (event) => {
     event.preventDefault();
+    // Show spinner while API resolves.
     setShowSpinner(true);
     try {
       const { data } = await axios.post(
         `${axiosDefaultsBaseUrl}dj-rest-auth/login/`,
         signInData,
       );
+      // If authenticated set user in browser.
       setCurrentUser(data.user);
+      // if authenticated save token to browser.
       setTokenTimestamp(data);
+      // If authenticated redirect user to their profile page.
       navigate(`/profile/${data.user.pk}`);
     } catch (err) {
+      // If not authenticated show expected error messages.
       setErrors(err.response?.data);
     } finally {
+      // Remove spinner.
       setShowSpinner(false);
     }
   };
 
+  /**
+   * Allow user to edit form.
+   * @param {*} e
+   */
   const handleChange = (event) => {
     setSignInData({
       ...signInData,
@@ -57,6 +76,9 @@ function SignInForm() {
     });
   };
 
+  /**
+   * Return JSX of component.
+   */
   return (
     <Container fluid className={styles}>
       <Row className="justify-content-center">
@@ -77,6 +99,7 @@ function SignInForm() {
               />
             </Form.Group>
 
+            {/*Display expected username error messages*/}
             {errors.username?.map((message, idx) => (
               <Alert key={idx} variant="warning">
                 {message}
@@ -96,13 +119,15 @@ function SignInForm() {
               />
             </Form.Group>
 
+            {/*Display expected password error messages*/}
             {errors.password?.map((message, idx) => (
               <Alert key={idx} variant="warning">
                 {message}
               </Alert>
             ))}
 
-            <div className="d-flex flex-row-reverse">
+            {/*Show spinner while waiting for API to resolve*/}
+            <div className="d-flex flex-row-reverse mb-3">
               {showSpinner ? (
                 <LoadSpinner />
               ) : (
@@ -114,10 +139,11 @@ function SignInForm() {
               )}
             </div>
 
-            <Link className={styles.Link} to="/signup">
+            <Link className={`${styles.Link}`} to="/signup">
               Don't have an account? <span>Sign up now!</span>
             </Link>
 
+            {/*Display expected non-field error messages*/}
             {errors.non_field_errors?.map((message, idx) => (
               <Alert key={idx} variant="warning" className="mt-3">
                 {message}
