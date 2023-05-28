6b47d65844e6922803b2564a9dc28fb65a6beb60
BobWritesCode <warwick_hart@hotmail.com>
2023-05-28 23:50:45 +0100

    tests: add UsernameUpdate render test

---
+++ src/components/profile/__tests__/UsernameUpdate.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import UsernameUpdate from '../UsernameUpdate';
+import { act } from 'react-dom/test-utils';
+import { CurrentUserProvider } from '../../../contexts/CurrentUserContext';
+
+describe('UsernameUpdate component test', () => {
+  const renderComponent = () =>
+    render(
+      <Router>
+        <CurrentUserProvider
+          value={{ currentUser: { username: 'mockUsername' } }}
+        >
+          <UsernameUpdate onUsernameChange={() => {}} />
+        </CurrentUserProvider>
+      </Router>,
+    );
+
+  it('renders the component', async () => {
+    renderComponent();
+    // This act makes sure that the render is completed because of any state changes after useEffect() API is resolved.
+    await act(async () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
