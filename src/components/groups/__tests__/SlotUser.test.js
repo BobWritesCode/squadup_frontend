10c85ff04af4b35384d460ed8dc4a7d8e2b590b4
BobWritesCode <warwick_hart@hotmail.com>
2023-05-28 21:44:30 +0100

    tests: add SlotUser render test

---
+++ src/components/groups/__tests__/SlotUser.test.js
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import SlotUser from '../SlotUser';
+import { act } from 'react-dom/test-utils';
+
+describe('SlotUser component test', () => {
+  const slotData = {
+    id: 228,
+  };
+  const renderComponent = () =>
+    render(
+      <Router>
+        <SlotUser slotData={slotData} />
+      </Router>,
+    );
+
+  it('renders the component', async () => {
+    renderComponent();
+    // This act makes sure that the render is completed because of any state changes after useEffect() API is resolved.
+    await act(async () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
