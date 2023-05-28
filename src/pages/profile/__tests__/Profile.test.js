796518174d2d7276248a10b99dddb31dc19ddf5a
BobWritesCode <warwick_hart@hotmail.com>
2023-05-29 00:24:47 +0100

    tests: add Profile render test

---
+++ src/pages/profile/__tests__/Profile.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router, useParams } from 'react-router-dom';
+import Profile from '../Profile';
+import { CurrentUserProvider } from '../../../contexts/CurrentUserContext';
+import { act } from 'react-dom/test-utils';
+import { ProfileDataProvider } from '../../../contexts/ProfileDataContext';
+
+// Mock useParams to provide the id parameter
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useParams: jest.fn(),
+}));
+
+describe('Profile component test', () => {
+  const renderComponent = () =>
+    render(
+      <Router>
+        <CurrentUserProvider>
+          <ProfileDataProvider>
+            <Profile />
+          </ProfileDataProvider>
+        </CurrentUserProvider>
+      </Router>,
+    );
+
+  it('renders the component', async () => {
+    // Mock Params to use.
+    useParams.mockReturnValue({ id: '28' });
+    renderComponent();
+    // This act makes sure that the render is completed because of any state changes after useEffect() API is resolved.
+    await act(async () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
