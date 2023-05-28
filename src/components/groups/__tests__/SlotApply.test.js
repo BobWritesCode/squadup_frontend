6af48fb1082ccb96174b6b303ca562efc46dd378
BobWritesCode <warwick_hart@hotmail.com>
2023-05-28 21:44:43 +0100

    tests: add SlotApply render test

---
+++ src/components/groups/__tests__/SlotApply.test.js
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import SlotApply from '../SlotApply';
+import { act } from 'react-dom/test-utils';
+
+describe('SlotApply component test', () => {
+  const renderComponent = () =>
+    render(
+      <Router>
+        <SlotApply />
+      </Router>,
+    );
+
+  it('renders the component', async () => {
+    renderComponent();
+    // This act makes sure that the render is completed because of any state changes after useEffect() API is resolved.
+    await act(async () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
