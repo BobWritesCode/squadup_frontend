c25f1df88e6a6cffaf176e60a6122586b0e7cd7a
BobWritesCode <warwick_hart@hotmail.com>
2023-05-28 22:12:26 +0100

    tests: add UserPosts render test

---
+++ src/components/posts/__tests__/UserPosts.test.js
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import UserPosts from '../UserPosts';
+import { act } from 'react-dom/test-utils';
+
+describe('UserPosts component test', () => {
+  const renderComponent = () =>
+    render(
+      <Router>
+        <UserPosts profileId={9} />
+      </Router>,
+    );
+
+  it('renders the component', async () => {
+    renderComponent();
+    // This act makes sure that the render is completed because of any state changes after useEffect() API is resolved.
+    await act(async () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
