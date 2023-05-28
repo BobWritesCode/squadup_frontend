a0687fe8f855d20b93277692eef2970d49fce169
BobWritesCode <warwick_hart@hotmail.com>
2023-05-28 22:35:22 +0100

    tests: add UserNote render test

---
+++ src/components/profile/__tests__/UserNote.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import UserNote from '../UserNote';
+import { act } from 'react-dom/test-utils';
+
+describe('UserNote component test', () => {
+  const renderComponent = () =>
+    render(
+      <Router>
+        <UserNote />
+      </Router>,
+    );
+
+  it('renders the component', async () => {
+    renderComponent();
+    // This act makes sure that the render is completed because of any state changes after useEffect() API is resolved.
+    await act(async () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
