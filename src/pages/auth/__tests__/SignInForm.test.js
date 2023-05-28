56d181f37cfe9ce8603822f7bb23e9ef5064aa1b
BobWritesCode <warwick_hart@hotmail.com>
2023-05-29 00:47:10 +0100

    tests: add SignInForm render test

---
+++ src/pages/auth/__tests__/SignInForm.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import SignInForm from '../SignInForm';
+
+// Mock the styles import
+jest.mock('../../styles/SignUpForm.module.css', () => ({}));
+
+describe('SignInForm component test', () => {
+  const renderComponent = () =>
+    render(
+      <Router>
+        <SignInForm />
+      </Router>,
+    );
+
+  it('renders the component', () => {
+    renderComponent();
+    expect(true).toEqual(true);
+  });
+});
