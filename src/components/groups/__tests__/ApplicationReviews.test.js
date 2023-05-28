0792f1994db7f2f003759ed5cf9bf4926d9da137
BobWritesCode <warwick_hart@hotmail.com>
2023-05-28 18:12:41 +0100

    tests: add render test for ApplicationReviews

---
+++ src/components/groups/__tests__/ApplicationReviews.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import ApplicationReviews from '../ApplicationReviews';
+
+describe('Application Reviews component test', () => {
+  // ID for mock api data used in handler.js
+  const slotData = {
+    id: 88,
+  };
+  const renderComponent = () =>
+    render(
+      <Router>
+        <ApplicationReviews slotData={slotData} />
+      </Router>,
+    );
+
+  it('renders the component', () => {
+    renderComponent();
+  });
+});
