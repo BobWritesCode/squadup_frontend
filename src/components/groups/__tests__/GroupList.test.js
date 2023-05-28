b68dfddc57f2c23cc4963762a4c5f9fbce14bf6f
BobWritesCode <warwick_hart@hotmail.com>
2023-05-28 21:14:56 +0100

    tests: add GroupList render test

---
+++ src/components/groups/__tests__/GroupList.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import GroupList from '../GroupList';
+import { CurrentUserProvider } from '../../../contexts/CurrentUserContext';
+import { act } from 'react-dom/test-utils';
+
+describe('GroupList component test', () => {
+  const renderComponent = () =>
+    render(
+      <Router>
+        <CurrentUserProvider>
+          <GroupList />
+        </CurrentUserProvider>
+      </Router>,
+    );
+
+  it('renders the component', async () => {
+    renderComponent();
+    // This act makes sure that the render is completed because of any state changes after useEffect() API is resolved.
+    await act(async () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
