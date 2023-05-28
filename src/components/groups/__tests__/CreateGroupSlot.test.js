79f27d40374abb197f6b028d3d75cc7df1022d58
BobWritesCode <warwick_hart@hotmail.com>
2023-05-28 18:40:17 +0100

    tests: add CreateGroupSlot render test

---
+++ src/components/groups/__tests__/CreateGroupSlot.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import CreateGroupSlot from '../CreateGroupSlot';
+
+describe('CreateGroupSlot component test', () => {
+  // Mock function for onSlotChange
+  const HandleOnSlotChange = () => {};
+
+  // Mock data for slotValue
+  const slotValue = {
+    role: 'Any',
+  };
+
+  const renderComponent = () =>
+    render(
+      <Router>
+        <CreateGroupSlot
+          onSlotChange={HandleOnSlotChange}
+          slotValue={slotValue}
+        />
+      </Router>,
+    );
+
+  it('renders the component', () => {
+    renderComponent();
+  });
+});
