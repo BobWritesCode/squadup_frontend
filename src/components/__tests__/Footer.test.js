d5f89948892bed8136b2d6f79e713e53cccd4f6f
BobWritesCode <warwick_hart@hotmail.com>
2023-05-28 16:51:52 +0100

    tests: add footer testing

---
+++ src/components/__tests__/Footer.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import Footer from '../Footer';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+describe('Check Footer rendering', () => {
+  const footerRender = () =>
+    render(
+      <Router>
+        <Footer />
+      </Router>,
+    );
+
+  it('checks social links show', () => {
+    footerRender()
+    const links = screen.getAllByRole('link');
+    expect(links[0]).toHaveAttribute('href', 'https://github.com/BobWritesCode');
+    expect(links[1]).toHaveAttribute('href', 'https://www.linkedin.com/in/warwickhart/');
+  });
+});
