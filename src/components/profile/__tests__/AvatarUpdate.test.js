350fac0fecccf7e42d0d83e0170280a21aea3acc
BobWritesCode <warwick_hart@hotmail.com>
2023-05-28 22:19:04 +0100

    tests: AvatarUpdate render test

---
+++ src/components/profile/__tests__/AvatarUpdate.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AvatarUpdate from '../AvatarUpdate';
+import { act } from 'react-dom/test-utils';
+
+describe('AvatarUpdate component test', () => {
+  const renderComponent = () =>
+    render(
+      <Router>
+        <AvatarUpdate />
+      </Router>,
+    );
+
+  it('renders the component', async () => {
+    renderComponent();
+    // This act makes sure that the render is completed because of any state changes after useEffect() API is resolved.
+    await act(async () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
