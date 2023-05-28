a9884f26dbe048f97ad72a5ce6e3feaef9f16a91
BobWritesCode <warwick_hart@hotmail.com>
2023-05-28 22:19:13 +0100

    tests: EmailUpdate render test

---
+++ src/components/profile/__tests__/EmailUpdate.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import EmailUpdate from '../EmailUpdate';
+import { act } from 'react-dom/test-utils';
+
+describe('EmailUpdate component test', () => {
+  const renderComponent = () =>
+    render(
+      <Router>
+        <EmailUpdate />
+      </Router>,
+    );
+
+  it('renders the component', async () => {
+    renderComponent();
+    // This act makes sure that the render is completed because of any state changes after useEffect() API is resolved.
+    await act(async () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
