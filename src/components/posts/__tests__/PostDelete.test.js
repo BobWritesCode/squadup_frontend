ef4c554ce218da7fcc12aeb70455f319bdda45c3
BobWritesCode <warwick_hart@hotmail.com>
2023-05-28 22:18:52 +0100

    tests: PostDelete render test

---
+++ src/components/posts/__tests__/PostDelete.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import PostDelete from '../PostDelete';
+import { act } from 'react-dom/test-utils';
+
+describe('PostDelete component test', () => {
+  // Mock data
+  const post = {
+    id: 1,
+    content: 'content',
+    image: '',
+  };
+  const renderComponent = () =>
+    render(
+      <Router>
+        <PostDelete post={post} />
+      </Router>,
+    );
+
+  it('renders the component', async () => {
+    renderComponent();
+    // This act makes sure that the render is completed because of any state changes after useEffect() API is resolved.
+    await act(async () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
