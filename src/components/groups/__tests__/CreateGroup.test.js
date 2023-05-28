7d5398c9345c1e0cc475351e6db9cfa8333e1f3f
BobWritesCode <warwick_hart@hotmail.com>
2023-05-28 18:40:28 +0100

    tests: add CreateGroup render test

---
+++ src/components/groups/__tests__/CreateGroup.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import CreateGroup from '../CreateGroup';
+
+describe('CreateGroup component test', () => {
+  const renderComponent = () =>
+    render(
+      <Router>
+        <CreateGroup />
+      </Router>,
+    );
+
+  it('renders the component', () => {
+    renderComponent();
+  });
+});
