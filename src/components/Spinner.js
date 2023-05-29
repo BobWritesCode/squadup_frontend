8e7bc459a269f4d752cc8530369cd07755655f23
BobWritesCode <warwick_hart@hotmail.com>
2023-05-29 17:12:20 +0100

    chore: add ability to choose variant in props

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Spinner from 'react-bootstrap/Spinner';
+import PropTypes from 'prop-types';
 
-const LoadSpinner = () => {
+const LoadSpinner = (props) => {
+  const {variant='light'} = props;
+
   return (
     <div className="d-flex justify-content-center">
-      <Spinner animation="border" role="status">
+      <Spinner animation="border" role="status" variant={variant}>
         <span className="visually-hidden">Loading...</span>
       </Spinner>
     </div>
   );
+};
+
+// Props validation
+LoadSpinner.propTypes = {
+  variant: PropTypes.string,
 };
 
 export default LoadSpinner;
