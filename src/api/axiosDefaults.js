d6662b6655c6ad902f54a2ec19e4b9da2f598b58
BobWritesCode <warwick_hart@hotmail.com>
2023-05-30 01:11:04 +0100

    chore: update default base url

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,4 +1,3 @@
-// export const axiosDefaultsBaseUrl = 'https://squadup-api.herokuapp.com/';
-export const axiosDefaultsBaseUrl = 'http://127.0.0.1:8000/';
+export const axiosDefaultsBaseUrl = 'https://squadup-api.herokuapp.com/';
 export const axiosDefaultsHeadersPostContentType = 'multipart/form-data';
 export const axiosDefaultsWithCredentials = true;
